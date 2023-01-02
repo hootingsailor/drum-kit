162e075a91f30773477dbfd9ee84707e136b6501
Aakash Sharma <sharmaaakash8924@gmai.com>
2023-01-02 23:18:00 +0530

    sound added

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-let btn = document.querySelectorAll(".drum");
-
-function handleClick(){
-    // alert("Clicked");
-    let audio = new Audio("sounds/crash.mp3");
-    audio.play();
+for(let i=0; i<document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 }
 
-for(let i=0; i<btn.length; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
-} 
+function handleClick(){
+    let btn = this.innerHTML;
+    switch(btn){
+        case "w":
+            let crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "a":
+            let kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "s":
+            let snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "d":
+            let tom_1 = new Audio("sounds/tom-1.mp3");
+            tom_1.play();
+            break;
+        case "j":
+            let tom_2 = new Audio("sounds/tom-2.mp3");
+            tom_2.play();
+            break;
+        case "k":
+            let tom_3 = new Audio("sounds/tom-3.mp3");
+            tom_3.play();
+            break;
+        case "l":
+            let tom_4 = new Audio("sounds/tom-4.mp3");
+            tom_4.play();
+            break;
+    }
+}
